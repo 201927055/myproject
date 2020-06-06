851abec844647c8a236c92c12f133bdcf72b9a5e
Y27055 <356mlnl@naver.com>
2020-06-06 18:16:51 +0900

    initial commit

--- app/src/main/java/com/youngjoo/myproject/MainActivity.java
+++ app/src/main/java/com/youngjoo/myproject/MainActivity.java
@@ -1,13 +1,34 @@
 package com.youngjoo.myproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private CalendarView mCalendarView;
+    private Button btnM;
+    private Button btnE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCalendarView = findViewById(R.id.calendarView);
+        btnM = findViewById(R.id.measure);
+        btnM = findViewById(R.id.exercise);
+
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
+                String date = year + "/" + month + "/" + day;
+                Log.d(TAG,"onSelectedDayChange : date :" + date);
+            }
+        });
+
     }
 }
