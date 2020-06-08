fe2a0478cda25ebe7a85fde08db9e5258e875b03
Y27055 <356mlnl@naver.com>
2020-06-08 14:30:55 +0900

    initial commit

--- app/src/main/java/com/youngjoo/myproject/BodyCheck.java
+++ app/src/main/java/com/youngjoo/myproject/BodyCheck.java
@@ -1,6 +1,18 @@
 package com.youngjoo.myproject;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class BodyCheck extends AppCompatActivity {
+        private static final String TAG = "BodyCheck";
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_bodycheck);
+
+                Intent incomingIntent = getIntent();
+
+        }
 }
--- app/src/main/java/com/youngjoo/myproject/MainActivity.java
+++ app/src/main/java/com/youngjoo/myproject/MainActivity.java
@@ -1,8 +1,10 @@
 package com.youngjoo.myproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 
@@ -27,6 +29,24 @@ import android.widget.CalendarView;
             public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
                 String date = year + "/" + month + "/" + day;
                 Log.d(TAG,"onSelectedDayChange : date :" + date);
+
+                Intent intent = new Intent(MainActivity.this, BodyCheck.class);
+                intent.putExtra("date",date);
+                startActivity(intent);
+            }
+        });
+
+        btnM.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, Measure.class);
+                startActivity(intent);
+            }
+        });
+
+        btnE.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, Exercise.class);
+                startActivity(intent);
             }
         });
 
