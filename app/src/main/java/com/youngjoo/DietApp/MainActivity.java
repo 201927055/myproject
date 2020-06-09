235a3f05de41466aad9ccf1aaf955c1b131b8531
Y27055 <356mlnl@naver.com>
2020-06-09 23:09:29 +0900

    initial commit

--- app/src/main/java/com/youngjoo/DietApp/MainActivity.java
+++ app/src/main/java/com/youngjoo/DietApp/MainActivity.java
@@ -3,32 +3,29 @@ package com.youngjoo.DietApp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 
     public class MainActivity extends AppCompatActivity {
-        private static final String TAG = "MainActivity";
 
-        private CalendarView mCalendarView;
-        private Button btnM;
-        private Button btnE;
+        CalendarView mCalendarView;
+        Button btnM;
+        Button btnE;
 
         @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
-        btnM = (Button) findViewById(R.id.measure);
-        btnE = (Button) findViewById(R.id.exercise);
+        mCalendarView = findViewById(R.id.calendarView);
+        btnM = findViewById(R.id.btnM);
+        btnE = findViewById(R.id.btnE);
 
         mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
                 String date = year + "/" + month + "/" + day;
-                Log.d(TAG,"onSelectedDayChange : date :" + date);
 
                 Intent intent = new Intent(MainActivity.this, BodyCheck.class);
                 intent.putExtra("date",date);
@@ -42,13 +39,11 @@ import android.widget.CalendarView;
                 startActivity(intent);
             }
         });
-
         btnE.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 Intent intent = new Intent(MainActivity.this, Exercise.class);
                 startActivity(intent);
             }
         });
-
     }
 }
